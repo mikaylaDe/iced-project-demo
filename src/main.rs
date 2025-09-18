b7e351d039e709863b5665e55f8056cb4c9b8185
Your Name <mikaylaforcollege@gmail.com>
2025-09-18 06:37:04 -0700

    update added water capacity tracker

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
-use iced::widget::{button, row, text};
+use iced::widget::{button, column, row, text};
 use iced::{Element, Result};
 
 #[derive(Default)]
 struct Plant {
     stage: u8,//holds values 0-255 (needs to account for the 
-               //rest of the values after 2 with _)
+               //rest of the values after 2 with _
+    water: u8, //water capacity left
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
     Watered,
+    Refill,
+    
 }
 
 impl Plant {
     fn update(&mut self, message: Message) {
-        if let Message::Watered = message {
-            if self.stage < 4 {
-                self.stage += 1;
+        match message {
+            Message::Watered => {
+                if self.water > 0 {
+                    if self.stage < 4 {
+                        self.stage += 1;
+                    }
+                    self.water -= 1;
+                }
+            }
+            Message::Refill => {
+                self.water = 3; //refills water to max
             }
         }
     }
@@ -27,12 +38,25 @@ impl Plant {
             1 => " 🌱",
             2 => " 🌿",
             3 => " 🪴",
-            _ => " 🌳", //accounts for values 2-255 (u8)
+            _ => " 🌳",
         };
+
+        let water = match self.water {
+            0 => " 🚰",     
+            1 => " 💧",
+            2 => " 💧💧",
+            _ => " 💧💧💧", 
+        };
+
         row![
-            button("Water").on_press(Message::Watered), 
-            text(plant).size(80), 
-            ]
+            column![
+                button("Water").on_press(Message::Watered),
+                button("Refill").on_press(Message::Refill),
+                //button("Reset plant"),on_press(Message:Reset),
+            ],
+            text(plant).size(80),
+            text(water).size(40),
+        ]
         .into()
     }
 }
